01c5776865ea6f20308e0e47cbd322a578d475f0
gos44 <gasc2004@gmail.com>
2025-05-21 11:21:08 -0500

    feat: add Button component and ProductHeader, update routing structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,8 @@
+<script>
+    import {Button} from '$components';
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
+<Button />
 <a href="/about">About</a>
--- src/routes/product/[id]/+page.svelte
+++ src/routes/product/[id]/+page.svelte
@@ -1,9 +1,11 @@
 <script>
     import {page} from '$app/stores'
+    import ProductHeader from './ProductHeader.svelte'
     $:console.log($page)
     
 </script>
-<h1>Product Page</h1>
+
+<ProductHeader />
 
 <p>Product ID:{$page.params.id}</p>
 <a href="/product/3450">Other product</a>
